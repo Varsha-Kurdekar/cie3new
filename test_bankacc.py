0693aa4aa6cf56ac79294b05ebd6821e1f6b64c5
Varsha-Kurdekar <varshavkurdekar@gmail.com>
2026-01-08 15:34:09 +0530

    save

---
+++ test_bankacc.py
@@ -0,0 +1,9 @@
+from bankacc import bankacc_details
+
+def test_bankacc_details():
+    expected_output = (
+        "Account Number: A101\n"
+        "Accont Name: Varsha\n"
+        "Type: Savings"
+    )
+    assert bankacc_details("A101", "Varsha", Savings) == expected_output
