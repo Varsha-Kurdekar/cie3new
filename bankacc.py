f207987e6788d123613cc5fdf4a9798d7c124e49
Varsha-Kurdekar <varshavkurdekar@gmail.com>
2026-01-08 15:27:00 +0530

    save

---
+++ bankacc.py
@@ -0,0 +1,15 @@
+def bankacc_details(account_number, name, type):
+    result = (
+        f"Account Number: {number}\n"
+        f"Account Name: {name}\n"
+        f"Account Type: {type}\n"
+    )
+    return result
+
+
+if __name__ == "__main__":
+    account_number = "A101"
+    name = "Varsha"
+    type = Savings
+
+    print(bankacc_details(account_number, name, type))
